a673058554dd33e6fa66e3c24c66ba44c457951d
TM1744 <marquesthiago1744@gmail.com>
2026-05-14 20:00:56 -0300

    feat/primeira atualização 14/05/2026

---
+++ gui.py
@@ -0,0 +1,48 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+import pygame
+
+from jogo_da_velha import criar_board, faz_movimento, get_input_valido, \
+    print_board, verifica_ganhador, verifica_movimento
+
+from mininmax import movimento_ia
+
+pygame.mixer.init()
+pygame.mixer.music.load('musica.mp3')
+pygame.mixer.music.play()
+
+pygame.font.init()
+
+def draw_board(win, board):
+    height = 600
+    width = 600
+    tamanho = 600/3
+
+    for i in range(1, 3):
+        pygame.draw.line(win, (0,0,0),
+                         (0,i * tamanho),
+                         (width, i, + tamanho), 3)
+
+        pygame.draw.line(win, (0, 0, 0),
+                         (i * tamanho, 0),
+                         (i * tamanho, height), 3)
+
+    for i in range(1, 3):
+        for j in range(1, 3):
+            font = pygame.font.SysFont('comicsans', 100)
+
+            x = j * tamanho
+            y = i * tamanho
+
+            text = font.render(board[i][j], 1, (0,0,0))
+            win.blit(text, ((x + 75), (y + 75)))
+
+def redraw_window(win, board):
+    win.fill((153, 51, 153))
+    draw_board(win, board)
+
+def main():
+    
---
+++ read.md
@@ -0,0 +1,11 @@
+# Selecionar o interpretador:
+
+---
+
+1. `CTRL + SHIFT + P`
+2. **Digite:**
+   - _Python: Select Interpreter_
+3. **Escolha:**
+   - _Enter interpreter path -> Find..._
+4. **Selecione a pasta:**
+   - `meu_projeto/python/python.exe`
